aa6c785fa94a330915dc357bf704d6d5a6e8bca1
Alberto Rossotto <alberto.rossotto@gmail.com>
2023-12-04 09:52:00 +0100

    with animation

--- src/main/java/advent2023/d04/Solution04.java
+++ src/main/java/advent2023/d04/Solution04.java
@@ -3,7 +3,10 @@ package advent2023.d04;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,6 +29,7 @@ public class Solution04 {
     }
 
     public int step2(List<String> input) {
+        Graphics graphics = createVisualisation();
         List<Card> originalCards = input.stream()
                 .map(l -> pattern.matcher(l))
                 .filter(Matcher::matches)
@@ -40,10 +44,48 @@ public class Solution04 {
                 int current = extra[i+c.getId()];
                 extra[c.getId()+i] = current + (1+bonus);
             }
+            visualise(extra, graphics, total);
         }
+        pause(10000);
         return total;
     }
 
+    private static final int HEIGHT = 500;
+    private static final int WIDTH = 1000;
+    private Graphics createVisualisation(){
+        JFrame frmMain = new JFrame();
+        frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmMain.setSize(WIDTH, HEIGHT);
+        Canvas cnvs = new Canvas();
+        cnvs.setSize(WIDTH, HEIGHT);
+        frmMain.add(cnvs);
+        frmMain.setVisible(true);
+        Graphics g = cnvs.getGraphics();
+        g.setColor(new Color(255, 255, 255));
+        return g;
+    }
+
+    private void visualise(int[] extra, Graphics graphics, int total){
+        int w = 50;
+        int h = 2;
+        for(int i=0;i<extra.length;i++){
+            graphics.setColor(new Color((i*13)%255, (i*19)%255, (i*31)%255));
+            graphics.fillRect(0,i*h,  ((int)Math.log(1+extra[i]))*w, h);
+        }
+        graphics.setColor(new Color(255, 255, 255));
+        graphics.fillRect(0,HEIGHT-100, WIDTH, 100);
+        graphics.setColor(new Color(0, 0, 255));
+        graphics.drawString("Total "+total, 100, HEIGHT-50);
+        pause(100);
+    }
+
+    private void pause(long l){
+        try {
+            Thread.sleep(l);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
 @Getter
