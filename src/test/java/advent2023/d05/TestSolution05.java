0c19a67470c006a9cff26de1dac07edc22975b2b
Alberto Rossotto <alberto.rossotto@gmail.com>
2023-12-09 02:53:00 +0100

    part1 (#7)

---
+++ src/test/java/advent2023/d05/TestSolution05.java
@@ -0,0 +1,25 @@
+package advent2023.d05;
+
+import advent2023.AbstractTest;
+import advent2023.d04.Solution04;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSolution05 extends AbstractTest {
+
+    @Test
+    public void testStep1() throws Exception {
+        List<String> input = getInput("input01.txt");
+        assertEquals(35L, new Solution05().step1(input));
+    }
+    @Test
+    public void testStep1actual() throws Exception {
+        List<String> input = getInput("input02.txt");
+        assertEquals(265018614L, new Solution05().step1(input));
+    }
+
+}
